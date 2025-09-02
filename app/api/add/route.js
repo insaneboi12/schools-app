ea1237849c50aa0b96f47862a1b6e1160d08051d
Meghsham15 <88832001+Meghsham15@users.noreply.github.com>
2025-09-02 11:25:56 +0530

    backend updates

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -1,72 +1,10 @@
 import { NextResponse } from "next/server";
 import mysql from "mysql2/promise";
-// const cert =process.env.DB_SSL_CERT.replace(/\\n/g, "\n");
-// console.log("cert", cert);
-
-const db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: parseInt(process.env.DB_PORT),
-    password: process.env.DB_PASSWORD,
-    database: "school_db",
-    ssl: {
-        ca: process.env.DB_SSL_CERT 
-      }
-});
-// console.log("dirName", process.cwd());
-
-// db.execute("CREATE DATABASE IF NOT EXISTS school_db");
-
-// create table - 
-// await db.execute("CREATE TABLE schools (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(100) NOT NULL,address TEXT,city VARCHAR(50),state VARCHAR(50),contact BIGINT NOT NULL UNIQUE,image TEXT,email_id VARCHAR(150) UNIQUE)");
-
-// insert data - 
-// This way not recommended because we are not using any prepared statement or any other security features to prevent SQL injection
-// await db.execute("INSERT INTO schools (name,address,city,state,contact,image,email_id) VALUES ('Vidyaranyapura school','Malleshwaram','Bangalore','Karnataka',9876543210,'image.jpg','vrs@gmail.com')");
-
-// Recommended way to insert data - 
-// await db.execute("INSERT INTO schools (name,address,city,state,contact,image,email_id) VALUES (?, ?, ?, ?, ?, ?, ?)", ["Vidyaranyapura school", "Malleshwaram", "Bangalore", "Karnataka", 9876543210, 'image.jpg', 'vrs@gmail.com']);
-
-// Multiple data insertion - 
 let values = [
     ["St. Joseph School", "Richmond Road", "Bangalore", "Karnataka", 9876543211, '', 'stj@gmail.com'],
     ["Delhi Public School", "Vasant Kunj", "New Delhi", "Delhi", 9876543212, '', 'dps@gmail.com'],
     ["Mount Carmel School", "Andheri West", "Mumbai", "Maharashtra", 9876543213, '', 'mcs@gmail.com'],
 ]
-// await db.query("INSERT INTO schools (name,address,city,state,contact,image,email_id) VALUES ?", [values]);
-// for large data we use query instead of execute
-
-// Return data - 
-// let dataA = await db.execute("SELECT * FROM schools");
-// console.log(dataA[0]);
-// in data we get an 2d array of in which array at 0 position is the data and 1 index position is the field names/ metadata
-// let data = await db.execute("SELECT * FROM schools where name='Vidyaranyapura school'");
-// console.log("data with where clause");
-// console.log(data[0]);
-
-// Update Query -
-// Not recommended code - 
-// try{
-//     let data = await db.execute("UPDATE schools SET image='newimage.jpg' WHERE id=1");
-//     console.log(data[0]);
-// }catch(error){
-//     console.log(error);
-// }
-
-// Recommended code - 
-// try{
-//     let data = await db.execute("UPDATE schools SET image=? WHERE id=?", ['newimage2.jpg', 2]);
-//     console.log(data[0]);
-// }catch(error){
-//     console.log(error);
-// }
-// Delete Query -
-// try{
-//     let data = await db.execute("DELETE FROM schools WHERE id=1");
-//     console.log(data[0]);
-// }catch(error){
-//     console.log(error);
-// }
 
 // console.log('mysql connected');
 
@@ -74,6 +12,16 @@ export async function POST(request) {
     let data = await request.json();
     // console.log(data);
     try{
+        const db = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            port: parseInt(process.env.DB_PORT),
+            password: process.env.DB_PASSWORD,
+            database: "school_db",
+            ssl: {
+                ca: process.env.DB_SSL_CERT 
+              }
+        });
     let dataA = await db.execute("INSERT INTO schools (name,address,city,state,contact,image,email_id) VALUES (?, ?, ?, ?, ?, ?, ?)", [data.name, data.address, data.city, data.state, data.contact, data.image, data.email]);
     // console.log("school added", dataA);
     return NextResponse.json({success: true, message: "School added successfully" });
@@ -85,6 +33,16 @@ export async function POST(request) {
 
 export async function GET() {
     try{
+        const db = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            port: parseInt(process.env.DB_PORT),
+            password: process.env.DB_PASSWORD,
+            database: "school_db",
+            ssl: {
+                ca: process.env.DB_SSL_CERT 
+              }
+        });
     let data = await db.execute("SELECT * FROM schools");
     // console.log("data",data[0].length);
     if(data[0].length<= 0){
@@ -102,6 +60,16 @@ export async function GET() {
 
 export async function DELETE(request) {
     try{
+        const db = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            port: parseInt(process.env.DB_PORT),
+            password: process.env.DB_PASSWORD,
+            database: "school_db",
+            ssl: {
+                ca: process.env.DB_SSL_CERT 
+              }
+        });
     let data = await request.json();
         // console.log(data);
     let dataA = await db.execute("DELETE FROM schools WHERE id=?", [data.id]);
